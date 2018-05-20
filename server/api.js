abbf628701bfa97124ff4984243f49d4ec068137
Preston Richey <prestonleerichey@gmail.com>
2018-05-20 12:47:17 -0500

    Wire up employee PATCH and DELETE routes

--- server/api.js
+++ server/api.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const router = new express.Router();
-
 const low = require('lowdb');
 const FileAsync = require('lowdb/adapters/FileAsync');
-const adapter = new FileAsync('db.json');
-
 const shortid = require('shortid');
+
+// normally I'd have a more robust directory structure, with services split out per model, but I figure this is fine for now
+const utils = require('./utils');
+
+const router = new express.Router();
+const adapter = new FileAsync('db.json');
 
 // get all employees
 router.get('/employees', async (req, res) => {
@@ -13,7 +15,7 @@ router.get('/employees', async (req, res) => {
     const db = await low(adapter);
     const employees = db.get('employees').value();
 
-    res.json(employees);
+    res.status(200).json(employees);
   } catch (err) {
     res.status(500).send(err.message);
   }
@@ -24,25 +26,66 @@ router.post('/employees', async (req, res) => {
   try {
     const db = await low(adapter);
 
-    if (!req.body.name) {
-      res.status(400).send('Employee must have a name!');
+    if (!utils.hasAllRequiredFields(req.body)) {
+      res
+        .status(400)
+        .send(`Must include all required fields: ${utils.requiredFields}`);
       return;
     }
 
-    const { name } = req.body; // only grab the things we need from the request
-    const newEmployee = { name, id: shortid() };
+    // naive attempt at only accepting valid fields in request
+    const sanitizedBody = utils.getSanitizedBody(req.body);
+    const newEmployee = { ...sanitizedBody, id: shortid.generate() };
 
-    await db
+    const postResponse = await db
       .get('employees')
       .push(newEmployee)
       .write();
 
-    res.sendStatus(200);
+    res.status(200).json(postResponse);
   } catch (err) {
     res.status(500).send(err.message);
   }
 });
 
-// TODO: fill out PATCH and DELETE /employee/:id
+// update employee
+router.patch('/employee/:id', async (req, res) => {
+  try {
+    const db = await low(adapter);
+
+    const { id } = req.params;
+    const employee = await db.get('employees').find({ id }); // chainable lowdb object
+
+    if (!employee.value()) {
+      res.status(400).send('Invalid ID');
+      return;
+    }
+
+    const sanitizedBody = utils.getSanitizedBody(req.body);
+    const patchResponse = await employee.assign(sanitizedBody).write();
+
+    res.status(200).json(patchResponse);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// remove employee
+router.delete('/employee/:id', async (req, res) => {
+  try {
+    const db = await low(adapter);
+
+    const { id } = req.params;
+
+    const deleteResponse = await db
+      .get('employees')
+      .remove({ id })
+      .write();
+
+    res.status(200).json(deleteResponse);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
 
 module.exports = router;
---
+++ server/utils.js
@@ -0,0 +1,44 @@
+const dbFields = [
+  {
+    key: 'name',
+    required: true
+  },
+  {
+    key: 'startDate',
+    required: false
+  },
+  {
+    key: 'department',
+    required: true
+  },
+  {
+    key: 'position',
+    required: true
+  }
+];
+
+// a more robust version of this would have individual field validation. I figure truthy is okay for now
+const _presentAndTruthy = (key, obj) => key in obj && !!obj[key];
+
+const validFields = dbFields.map(field => field.key);
+
+const requiredFields = dbFields
+  .filter(field => field.required)
+  .map(field => field.key);
+
+const hasAllRequiredFields = body =>
+  requiredFields.every(field => _presentAndTruthy(field, body));
+
+// iterate over valid fields and create new obj with those found in request body
+const getSanitizedBody = body =>
+  validFields.reduce(
+    (acc, field) =>
+      _presentAndTruthy(field, body) ? { ...acc, [field]: body[field] } : acc,
+    {}
+  );
+
+module.exports = {
+  requiredFields,
+  hasAllRequiredFields,
+  getSanitizedBody
+};
